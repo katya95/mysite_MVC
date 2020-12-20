5faa222244cdea1847fb6350fee023d64a597cd0
katya95 <73447171+katya95@users.noreply.github.com>
2020-12-21 00:23:38 +0200

    Add files via upload

---
+++ mysite mvc/application/models/model_insert.php
@@ -0,0 +1,43 @@
+﻿<?php
+
+class Model_insert extends Model
+{
+protected $db;
+    public function __construct($db) {
+        $this->db = $db;
+        
+        
+    }
+	public function get_data()
+	{	
+		
+		
+		$query = "INSERT INTO `user` (
+  `name`,
+  `surname`,
+  `city`,
+  `email`,
+  `password`
+  )
+VALUES (
+  :name,
+  :surname,
+  :city,
+  :email,
+  :password
+)";
+ 
+$args = [
+  'name' => $_POST['name'],
+  'surname' => $_POST['name_2'],
+  'city' => $_POST['city'],
+  'email' =>$_POST['email'],
+  'password' =>$_POST['password'],
+'avatar'=>''];
+ 
+ $this->db->sql($query, $args);
+		
+	}
+
+}
+?>
