740dba3852003869a180892ef2edbccdf0213d64
katya95 <73447171+katya95@users.noreply.github.com>
2021-01-25 02:28:49 +0200

    Add files via upload

--- application/models/model_post.php
+++ application/models/model_post.php
@@ -10,24 +10,38 @@ class ModelPost extends Model
     // posts output
 	public function getData()
 	{	
-    return $post=$this->db->getRows("SELECT * FROM post");
+  		return $post=$this->db->getRows("SELECT * FROM post");
 	}
 
 //Active Record
+	public function getId($id)
+	{
+		return $this->db->getValue("SELECT `id_post` FROM `post` WHERE  `id_post` = ? AND  `id_post`!=''", [$id]);
+	}
 	public function getTitle($id)
 	{
-		return $this->db->getValue("SELECT `title` FROM `post` WHERE `id_post` > ?", [$id]);
+		return $this->db->getValue("SELECT `title` FROM `post` WHERE `id_post` = ?", [$id]);
 	}
 
 	public function getText($id)
 	{
-		return $this->db->getValue("SELECT `text` FROM `post` WHERE `id_post` > ?", [$id]);
+		return $this->db->getValue("SELECT `text` FROM `post` WHERE `id_post` = ?", [$id]);
 	}
 	
 	public function getCount()
 	{
 		return $this->db->getValue("SELECT COUNT(1) FROM `post`");
 	}
+	public function getUser($id)
+	{
+		return $this->db->getValue("SELECT (SELECT  `name`  FROM `user`  WHERE `id_user`  = post.id_user), `id_post` FROM `post` WHERE `id_post` = ? ", [$id]);
+	}
+	
+	public function getSurname($id)
+	{
+		return $this->db->getValue("SELECT (SELECT  `surname`  FROM `user`  WHERE `id_user`  = post.id_user), `id_post` FROM `post` WHERE `id_post` = ? ", [$id]);
+	}
+
 }
 
 ?>
