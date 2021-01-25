fad0ca58abc1c3cf97751aa1f2fcdf648f80c00d
katya95 <73447171+katya95@users.noreply.github.com>
2021-01-25 02:32:34 +0200

    Add files via upload

---
+++ application/controllers/controller_user.php
@@ -0,0 +1,20 @@
+﻿<?php
+ 
+class Controller_User extends Controller
+{
+	//linking the DB and model, which implements data output using PDO
+	protected $db;
+    public function __construct()
+     {
+         $this->view = new View();
+         $this->db = new DB();
+         $this->model = new ModelUser( $this->db);
+    }
+	//for output page "user" using general template
+	function action_index()
+	{
+		$data = $this->model->getData();		
+		$this->view->generate('user_view.php', 'template_view.php', $data);
+	}
+
+}
