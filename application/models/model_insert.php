890284cc986091d814be828b1b987ae895e1ee83
katya95 <73447171+katya95@users.noreply.github.com>
2021-01-25 02:28:08 +0200

    Add files via upload

--- application/models/model_insert.php
+++ application/models/model_insert.php
@@ -38,63 +38,65 @@ class ModelInsert extends Model
   		'password' =>$_POST['password'],
 		'avatar'=>NULL
 		];
-        $this->db->sql($query, $args);	*/
+ $this->db->sql($query, $args);	*/
 	}
 	
 	//Option 2. Using ActiveRecord
  protected $name;
- protected $name_2;
- protected $city;
- protected $email;
- protected $password;
+   protected $name_2;
+    protected $city;
+    protected $email;
+   protected $password;
+   protected $check;
+   public $answer;
       //insert name
     public  function setName($name)
     {
-        $this->name = $name;
+    $this->name = $name;
     }
     public function getName()
     {
-        return $this->name;
+    return $this->name;
     }
     //insert surname
 	public  function setSurname($name_2)
 	{
-        $this->name_2 = $name_2;
+    $this->name_2 = $name_2;
     }
-    public function getSurname()
+public function getSurname()
     {
-        return $this->name_2;
+    return $this->name_2;
     }
       //insert city
 	public  function setCity($city)
 	{
-        $this->city = $city;
+    $this->city = $city;
     }
-    public function getCity()
+public function getCity()
     {
     return $this->city;
     }
       //insert email
 	public  function setEmail($email)
 	{
-        $this->email = $email;
+    $this->email = $email;
     }
-    public function getEmail()
+public function getEmail()
     {
-        return $this->email;
+    return $this->email;
     }
       //insert password
 	public  function setPassword($password)
 	{
-        $this->password = $password;
+    $this->password = $password;
     }
-    public function getPassword()
+public function getPassword()
     {
-        return $this->password;
+    return $this->password;
     }
     //insert data in database
     public function insert() {
-        $query = "INSERT INTO `user` (
+    $query = "INSERT INTO `user` (
 		`id_user`,	
  		 `name`,
  		 `surname`,
@@ -112,7 +114,7 @@ class ModelInsert extends Model
 		  :password,
 		  :avatar
 		)";
- 
+    
 		$args = [
 		'id_user'=>NULL,
  		 'name' => $this->name,
@@ -122,10 +124,52 @@ class ModelInsert extends Model
   		'password' =>$this->password,
 		'avatar'=>NULL
 		];
-			
-        $this->db->sql($query, $args);	
+ $this->db->sql($query, $args);	
     }
-    
+    public function check()
+    {
+    if( isset ($_POST['name']) && trim(strlen($_POST['name'])) > 0 ){
+    $this->setName($_POST['name']);
+    $name=1;
+    }else {
+	$this->answer='Введите имя!';
+	$name=0;
+	echo $this->answer;
+	}
+    if( isset ($_POST['name_2']) && trim(strlen($_POST['name_2'])) > 0 ){
+    $this->setSurname($_POST['name_2']);
+    $name_2=1;
+    }else {
+	$name_2=0;
+    }
+    if( isset ($_POST['city']) && trim(strlen($_POST['city'])) > 0 ){
+    $this->setCity($_POST['city']);
+    $city=1;
+    }else {
+	$city=0;
+    }
+    if( isset ($_POST['email']) && trim(strlen($_POST['email'])) >  0){
+		if (preg_match("/^(?:[a-z0-9]+(?:[-_.]?[a-z0-9]+)?@[a-z0-9_.-]+(?:\.?[a-z0-9]+)?\.[a-z]{2,5})$/i", $_POST['email'])){
+    $this->setEmail($_POST['email']);
+    $email=1;
+		}else {
+		print  ("Емейл указан неверно!");
+		$email=0;
+		}
+    } else {
+    $email=0;
+    }
+    if( isset ($_POST['password']) && trim(strlen($_POST['password'])) > 0 ){
+    $this->setPassword($_POST['password']);
+    $password=1;
+    }else {
+    $password=0;
+    }
+     	if ($name==1 && $name_2==1 && $city==1 && $email==1 && $password==1){
+      $this->insert(); 
+      $this->answer="Вы зарегистрированы в системе!";
+   		}
+	}
+	
 }
-
 ?>
