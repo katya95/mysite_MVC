b8cf70da1d7e238ee2d46a74d1a510d34d508890
katya95 <73447171+katya95@users.noreply.github.com>
2021-01-25 02:36:13 +0200

    Add files via upload

---
+++ application/views/user_view.php
@@ -0,0 +1,18 @@
+﻿<h1>Пользователи</h1>
+<p>
+<table width='80%'><tr><td>
+<b>Вывод пользователей</b>
+</td></tr>
+<?php 
+//output posts
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(E_ALL);
+$db = new DB();
+$user=new ModelUser($db);
+echo  '<tr><td>';
+ $user->getIdUser();
+echo '</td></tr></table>';
+
+   		
+?>
