383fb5ffe4112c2c543d2ccfedf6bd5c6e05b151
katya95 <73447171+katya95@users.noreply.github.com>
2021-01-25 02:35:17 +0200

    Add files via upload

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -1,31 +1,16 @@
 ﻿  <h1>Профиль</h1>
-
 <?php
-		class Profile 
-{
-    protected $db;
-    public function __construct($db)
-    {
-        $this->db = $db;
-    }
-    //page profile of registered user- output data from database 'mysite' 
-	public function get_post()
-	{	
-	
-		$id = file_get_contents('application/views/content2_view.php');
-		$this->db->getRow("SELECT * FROM user  WHERE `id_user` = ?", [$id]);
-		$user= $this->db->getRow("SELECT * FROM user  WHERE `id_user` = ?", [$id]);
-		
-		echo 'Имя:'.' '.$user['name'].'<br/>';
-		echo 'Фамилия:'.' '.$user['surname'].'<br/>';
-		echo 'Город:'.' '.$user['city'].'<br/>';
-		echo 'Е-мейл:'.' '.$user['email'].'<br/>';
-		echo 'Фотография:'.' <img src="application/foto/'.$user['avatar'].'"width=30%>'.'<br/>';
-	}
-}
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(E_ALL);
 $db = new DB();
-$test = new Profile($db);
-$data = $test->get_post();
-		
+$user=new ModelProfile($db);
+$id = file_get_contents('application/views/content2_view.php');
+echo 'Имя:'.' '.$user->getName($id).'<br/>';
+echo 'Фамилия:'.' '.$user->getSurname($id).'<br/>';
+echo 'Город:'.' '.$user->getCity($id).'<br/>';
+echo 'Е-мейл:'.' '.$user->getEmail($id).'<br/>';
+echo 'Фотография:'.' <img src="application/foto/'.$user->getAvatar($id).'"width=30%>'.'<br/>';
+
 ?>
 
