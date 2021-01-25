d1db4542da7abf3390613e9919424495c762efd1
katya95 <73447171+katya95@users.noreply.github.com>
2021-01-25 16:19:21 +0200

    Add files via upload

--- application/models/model_user.php
+++ application/models/model_user.php
@@ -13,38 +13,13 @@ class ModelUser extends Model
   		return $user=$this->db->getRows("SELECT * FROM user");
 	}
 
-//Active Record
-	public function getId()
-	{
-		return $this->db->getColumn("SELECT `id_user` FROM `user`");
-	}
-	public function getName($id)
-	{
-		return $this->db->getValue("SELECT `name` FROM `user` WHERE `id_user` = ?", [$id]);
-	}
-
-	public function getSurname($id)
-	{
-		return $this->db->getValue("SELECT `surname` FROM `user`WHERE `id_user` = ?", [$id]);
-	}
-	
-	public function getCity($id)
-	{
-		return $this->db->getValue("SELECT `city` FROM `user` WHERE `id_user` = ?", [$id]);
-	}
-	
-	public function getEmail($id)
-	{
-		return $this->db->getValue("SELECT `email` FROM `user` WHERE `id_user` = ?", [$id]);
-	}
-	
 	public function getIdUser()
 	{
- 		$data=$this->getId();
+ 		$data=$this->getIdModel();
 		foreach ($data as $item) {
-			echo 'Пользователь '.$this->getName($item).' '. $this->getSurname($item).'<br/>';
-			echo 'Город: '.$this->getCity($item).'<br/>';
-			echo 'Емейл: '.$this->getEmail($item).'<br/>';
+			echo 'Пользователь '.$this->getNameModel($item).' '. $this->getSurnameModel($item).'<br/>';
+			echo 'Город: '.$this->getCityModel($item).'<br/>';
+			echo 'Емейл: '.$this->getEmailModel($item).'<br/>';
   			echo '<br/>';
 		}
 	}	
