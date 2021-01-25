f53d19c36836551a0a5c878385c76374d3ca9d1e
katya95 <73447171+katya95@users.noreply.github.com>
2021-01-25 16:19:55 +0200

    Add files via upload

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -6,10 +6,10 @@ error_reporting(E_ALL);
 $db = new DB();
 $user=new ModelProfile($db);
 $id = file_get_contents('application/views/content2_view.php');
-echo 'Имя:'.' '.$user->getName($id).'<br/>';
-echo 'Фамилия:'.' '.$user->getSurname($id).'<br/>';
-echo 'Город:'.' '.$user->getCity($id).'<br/>';
-echo 'Е-мейл:'.' '.$user->getEmail($id).'<br/>';
+echo 'Имя:'.' '.$user->getNameModel($id).'<br/>';
+echo 'Фамилия:'.' '.$user->getSurnameModel($id).'<br/>';
+echo 'Город:'.' '.$user->getCityModel($id).'<br/>';
+echo 'Е-мейл:'.' '.$user->getEmailModel($id).'<br/>';
 echo 'Фотография:'.' <img src="application/foto/'.$user->getAvatar($id).'"width=30%>'.'<br/>';
 
 ?>
