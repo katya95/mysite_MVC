6b265c594d0084dd4c362c0cef2134b82754a91f
katya95 <73447171+katya95@users.noreply.github.com>
2021-01-25 02:35:39 +0200

    Add files via upload

--- application/views/update_view.php
+++ application/views/update_view.php
@@ -1,31 +1,9 @@
 ﻿<?php 
 include_once ('application/models/model_insert.php');
 include_once ('application/config.php');
-$back = "<meta http-equiv='refresh' content='0; {$_SERVER['HTTP_REFERER']} ' ";
-$answer='';
 $db = new DB();
 $update=new ModelUpdate($db);
-if( isset ($_POST['name']) && trim(strlen($_POST['name'])) > 0 ){
-	$update-> setNameActive($_POST['name']);
-}
-if( isset ($_POST['name_2']) && trim(strlen($_POST['name_2'])) > 0 ){
-	$update-> setSurnameActive($_POST['name_2']);
-}
-if( isset ($_POST['city']) && trim(strlen($_POST['city'])) > 0 ){
-	$update-> setCityActive($_POST['city']);
-}
-if( isset ($_POST['email']) && trim(strlen($_POST['email'])) >  0){
-		if (preg_match("/^(?:[a-z0-9]+(?:[-_.]?[a-z0-9]+)?@[a-z0-9_.-]+(?:\.?[a-z0-9]+)?\.[a-z]{2,5})$/i", $_POST['email'])){
-			$update-> setEmailActive($_POST['email']);
-		}else {
-			$answer= 'Емейл указан неверно! ';
-			echo $back;
-		}
-}
-if( isset ($_POST['password']) && trim(strlen($_POST['password'])) > 0 ){
-	$update-> setPasswordActive($_POST['password']);
-}
-$update->updateUserActive();
+$update->checkUpdate();
 $answer='Данные отправлены';
 ?>
 
