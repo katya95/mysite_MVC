f9e1ea8c237562fc74eab3879f6020f661019514
katya95 <73447171+katya95@users.noreply.github.com>
2021-01-25 16:18:03 +0200

    Add files via upload

--- application/core/model.php
+++ application/core/model.php
@@ -8,9 +8,28 @@ class Model
 		// todo
 	}
 	//The field "id_user" is index in database and used in all tables
-public function getIdModel()
+	public function getIdModel()
 	{
 		return $this->db->getColumn("SELECT `id_user` FROM `user`");
+	}
+	public function getNameModel($id)
+	{
+		return $this->db->getValue("SELECT `name` FROM `user` WHERE `id_user` = ?", [$id]);
+	}
+
+	public function getSurnameModel($id)
+	{
+		return $this->db->getValue("SELECT `surname` FROM `user`WHERE `id_user` = ?", [$id]);
+	}
+	
+	public function getCityModel($id)
+	{
+		return $this->db->getValue("SELECT `city` FROM `user` WHERE `id_user` = ?", [$id]);
+	}
+	
+	public function getEmailModel($id)
+	{
+		return $this->db->getValue("SELECT `email` FROM `user` WHERE `id_user` = ?", [$id]);
 	}
 	
 }
