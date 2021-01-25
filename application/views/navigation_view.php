e09b3d67897b2cdcf44a877d6c75d72bbbf45da1
katya95 <73447171+katya95@users.noreply.github.com>
2021-01-25 02:34:17 +0200

    Add files via upload

--- application/views/navigation_view.php
+++ application/views/navigation_view.php
@@ -3,6 +3,7 @@
 			<li><a href="/postarray">Пост</a></li>
 			<li><a href="/test">Регистрация</a></li>
 			<li><a href="/post">пост бд</a></li>
+			<li><a href="/user">Пользователи</a></li>
 			<?php 
 			$a = file_get_contents('application/views/content2_view.php');
 	 		if ($a!=0){
